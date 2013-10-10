edd3d4e3a19d764e48332c0f723b3581fa423bb5
Randy Lubin <randylubin@gmail.com>
2013-10-10 11:25:12 -0700

    change signalmaster back

--- scripts/simplewebrtc.bundle.js
+++ scripts/simplewebrtc.bundle.js
@@ -12,7 +12,7 @@ function SimpleWebRTC(opts) {
     var self = this;
     var options = opts || {};
     var config = this.config = {
-            url: 'http://signaling.randylubin.com:8888',
+            url: 'http://signaling.simplewebrtc.com:8888',
             debug: false,
             localVideoEl: '',
             remoteVideosEl: '',
